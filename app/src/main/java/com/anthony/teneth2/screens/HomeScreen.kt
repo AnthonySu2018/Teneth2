e7575071bfee2f0a7efa09a26ef40c906bde2702
AnthonySu <anthonysu91@gmail.com>
2024-05-19 11:17:59 +0800

    home page is done

--- app/src/main/java/com/anthony/teneth2/screens/HomeScreen.kt
+++ app/src/main/java/com/anthony/teneth2/screens/HomeScreen.kt
@@ -1,8 +1,20 @@
 package com.anthony.teneth2.screens
 
+import android.webkit.WebSettings
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.viewinterop.AndroidView
 
 @Composable
 fun HomeScreen() {
-    // Add your home screen content here
+    AndroidView(factory = { context ->
+        WebView(context).apply {
+            settings.cacheMode = WebSettings.LOAD_NO_CACHE
+            webViewClient = WebViewClient()
+            loadUrl("http://www.teneth.net/EN/ABOUT%20US/")
+
+        }
+    })
+
 }
